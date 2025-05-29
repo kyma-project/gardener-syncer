482e749c4147b9ef4df18d1de0b93730d441204e
Przemysław Golicz <przemyslaw.golicz@sap.com>
2025-05-29 12:06:21 +0200

    Add function to stop istio sidecar on exit

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	log "log/slog"
+	"net/http"
+	"net/url"
 	"os"
 
 	cli "github.com/kyma-project/gardener-syncer/internal"
 )
 
 func main() {
+	defer haltIstioSidecar()
+
 	if err := cli.Run(); err != nil {
 		log.Error("application failed", "error", err.Error())
 		os.Exit(1)
+	}
+}
+
+func haltIstioSidecar() {
+	log.Info("# HALT ISTIO SIDECAR #")
+	resp, err := http.PostForm("http://127.0.0.1:15020/quitquitquit", url.Values{})
+
+	if err != nil {
+		log.Error("unable to send post request to quit Istio sidecar", "error", err)
+		return
+	}
+
+	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+		log.Info("Stopping istio sidecar, ", "response status", resp.StatusCode)
+		return
 	}
 }
