370dfc12f701d40438f824d379b001a48dac4368
Przemysław Golicz <przemyslaw.golicz@sap.com>
2025-05-29 12:46:09 +0200

    Moving haltIstioSidecar() deferred call to main Run() function

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	log "log/slog"
-	"net/http"
-	"net/url"
 	"os"
 
 	cli "github.com/kyma-project/gardener-syncer/internal"
@@ -12,7 +10,6 @@ import (
 func main() {
 	if err := cli.Run(); err != nil {
 		log.Error("application failed", "error", err.Error())
-		haltIstioSidecar() // os.Exit is not called in defer, so we need to call it here as well
 		os.Exit(1)
 	}
 }
